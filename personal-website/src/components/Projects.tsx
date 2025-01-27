61de40d41e42464afb0549a9795aa2440197d45b
Akhanali <akhanali.amangeldy@gmail.com>
2025-01-27 15:31:21 -0500

    dexscription ab projects

--- personal-website/src/components/Projects.tsx
+++ personal-website/src/components/Projects.tsx
@@ -9,7 +9,7 @@ const Projects = () => {
   return (
     <div id = 'projects' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
       <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Projects</h1>
-      <p className='text-center py-8'>
+      <p className='text-center py-8'>  Here are some of the projects I’ve worked on, showcasing my skills in web development, problem-solving, and creating user-friendly applications. Each project demonstrates my ability to learn and implement different technologies, from frontend to backend development.
       </p>
       <div className='grid sm:grid-cols-2 gap-12'>
         <ProjectItem img={guessImg} title='Guess The Number' src='https://akhanali.github.io/GuessTheNumber/' info='HTML | CSS | JavaScript'/>
